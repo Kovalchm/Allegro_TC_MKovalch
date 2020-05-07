9e1f2a819e39db8b00ba0ed6d7b687098e97c2b5
misha <kmmmmk770@gmail.com>
2020-05-07 22:16:14 +0200

    Test_task

--- src/main/java/SQL_HELPER/DB_Manager.java
+++ src/main/java/SQL_HELPER/DB_Manager.java
@@ -1,4 +1,9 @@
 package SQL_HELPER;
 
 public class DB_Manager {
+    public static String url= "";
+    public static String user = "postgres";
+    public static String passwd = "1Qwerty1";
+
+
 }
